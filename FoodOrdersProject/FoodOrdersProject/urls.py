451ef5cd169ed8276f7a8d78c9992650deaa8e80
Gayuh <ahmadsgr39@gmail.com>
2024-05-02 18:50:10 +0700

    Add example for serializers for custom validation and rename Master => master, Authentication => authentication

--- FoodOrdersProject/FoodOrdersProject/urls.py
+++ FoodOrdersProject/FoodOrdersProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Master.urls'))
+    path('api/', include('master.urls'))
 ]
--- FoodOrdersProject/Master/views.py
+++ FoodOrdersProject/Master/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer
-from . import models
+from rest_framework.decorators import api_view
 from FoodOrdersProject import utils
+from .serializer import Test
 import logging
 
 # Create your views here.
@@ -13,7 +10,10 @@ logger = logging.getLogger(__name__)
 @api_view(["GET"])
 def index(request, *args, **kwargs):
     if request.method == "GET":
-        queryset = models.Categories.objects.all()
-        logger.info("Test")
-        logger.info(queryset)
-        return utils.generic_response(message="Oke", status_code=200)
+        data = {
+            "name": ""
+        }
+
+        test = Test(data=data)
+        logger.info(test.is_valid(raise_exception=True))
+        return utils.generic_response(message="Oke", status_code=200, data=test.data)
