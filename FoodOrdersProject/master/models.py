48fa4b65fb4962a77f4096b0ae60568634e85436
Gayuh <ahmadsgr39@gmail.com>
2024-05-04 17:40:24 +0700

    Add back the folder with lower case name

---
+++ FoodOrdersProject/master/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+class Categories(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(null=False, max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.id} => {self.name}'
+
+    class Meta:
+        db_table = '"food_orders"."categories"'
+
+class Medias(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(null=False, max_length=200)
+    type = models.CharField(null=False, max_length=100)
+    size = models.IntegerField(null=False)
+
+    def __str__(self) -> str:
+        return f'{self.id} => {self.name}'
+
+    class Meta:
+        db_table = '"food_orders"."medias"'
+
+
+class PaymenMethod(models.Model):
+    class PrefixChoices(models.TextChoices):
+        BANK = 'BNK', 'Bank'
+        GOJEK = 'GJK', 'Gojek'
+        DANA = 'DNA', 'Dana'
+        OVO = 'OVO', 'Ovo'
+        SHOPEE = 'SHP', 'Shopee'
+
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(null=False, max_length=200)
+    prefix_code = models.CharField(default='', max_length=3, null=False, choices=PrefixChoices.choices)
+
+    def __str__(self) -> str:
+        return f'{self.prefix_code} => {self.name}'
+
+    class Meta:
+        db_table = '"food_orders"."payment_methods"'
+
+class Promos(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(null=False, max_length=200)
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+
+    def __str__(self) -> str:
+        return f'{self.id} => {self.name}'
+
+    class Meta:
+        db_table = '"food_orders"."promos"'
+
+class HomeBanners(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(max_length=200, null=False)
+    media = models.OneToOneField(Medias, on_delete=models.RESTRICT, db_column='media_id', null=False)
+
+    def __str__(self) -> str:
+        return f'{self.id} => {self.name}'
+    
+    class Meta:
+        db_table = '"food_orders"."home_banners"'
---
+++ FoodOrdersProject/master/serializer.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class Test(serializers.Serializer):
+    name = serializers.CharField(allow_blank=False, error_messages={'blank': 'Harus diisi ngab'})
+    age = serializers.IntegerField(max_value=10, required=True)
