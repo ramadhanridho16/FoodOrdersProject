327904b6ace0df2b5245fb7009729aa814142f05
Gayuh <ahmadsgr39@gmail.com>
2024-05-09 23:14:46 +0700

    Add async view as example
    Async view that takes 2 second until the second logger is appear

--- FoodOrdersProject/master/urls.py
+++ FoodOrdersProject/master/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('async/<str:name>/', views.test_async),
 ]
--- FoodOrdersProject/master/views.py
+++ FoodOrdersProject/master/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from asgiref.sync import sync_to_async
 from FoodOrdersProject import utils
 from .serializer import Test
 import logging
+import asyncio
+import time
+from subprocess import call
 
 # Create your views here.
 
@@ -10,6 +15,8 @@ logger = logging.getLogger(__name__)
 @api_view(["GET"])
 def index(request, *args, **kwargs):
     if request.method == "GET":
+        print(kwargs)
+        logger.info("Tetsing")
         data = {
             "name": ""
         }
@@ -17,3 +24,17 @@ def index(request, *args, **kwargs):
         test = Test(data=data)
         logger.info(test.is_valid(raise_exception=True))
         return utils.generic_response(message="Oke", status_code=200, data=test.data)
+    
+
+async def test_async(request, *args, **kwargs):
+    if request.method == "GET":
+        name = kwargs['name']
+        asyncio.create_task(testing_sleep(name))
+        return JsonResponse(status=201, data={"Message":name})
+    
+
+@sync_to_async(thread_sensitive=False)
+def testing_sleep(name):
+    logger.info(name)
+    time.sleep(2)
+    logger.info(name)
