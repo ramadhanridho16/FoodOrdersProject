6eeb69e67237dfd2a46a643000473da0b47bebc8
Gayuh <ahmadsgr39@gmail.com>
2024-07-07 01:29:45 +0700

    Update static text - Add static_message file to store all static message from response to email

---
+++ FoodOrdersProject/static_message.py
@@ -0,0 +1,21 @@
+# Message Response
+SUCCESS_REGISTER = "Register success, check your email for account verification"
+FAILED_REGISTER = "Register failed, please provide the correct data"
+DESTINATION_NOT_VALID = "Destination is not valid"
+WRONG_FORMAT_FILE = "Wrong format file, make sure it's a image format"
+FILE_TOO_LARGE = "File too large, max size is {0} Mb"
+SERVER_ERROR = "Something wrong with the server"
+USED_USERNAME_EMAIL = "Username or email already used"
+PASSWORD_NOT_SAME = "Password and confirmation password is not same"
+WRONG_GENDER = "Wrong gender"
+PATH_NOT_FOUND = "Path {0} not found"
+BAD_REQUEST = "Bad request"
+
+# Email event
+REGISTER = "REGISTER"
+RESEND_VERIFY = "RESEND_VERIFY"
+FORGET_PASSWORD = "FORGET_PASSWORD"
+
+# Email
+SUBJECT = "Pinara Account Verification"
+BODY = "Halo {0}, silakan klik tombol di bawah untuk melakukan verifikasi akun sebelum {1}."
